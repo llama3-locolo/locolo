adca1d324f6e23518d2a21851097e01100c0d1ab
Sean Chiang <seanchiang226@gmail.com>
2024-05-11 21:17:54 -0700

    Updated prompt

--- app/api/chat/engine/chat.ts
+++ app/api/chat/engine/chat.ts
@@ -17,12 +17,17 @@ export async function createChatEngine() {
     chatModel: Settings.llm,
     retriever,
     contextSystemPrompt({ context }) {
-      return `Context information is below.
+      return `Context information is below. 
+      The context is a large array of text files with information regarding Eventbrite events for the month of May in the year 2024.
       ---------------------
       ${context}
       ---------------------
-      Given the context information and not prior knowledge, answer the query.
-      Only answer in JSON.
+      You are an event curator, with a database of Eventbrite events to reference.
+      Your role is to suggest events that you believe the user will enjoy based on their queries.
+      Answer the query with the given context as your source of truth. 
+      You may use prior knowledge to interpret the user's query, but not in forming your response.
+      You must only answer with events that are referenced in the context, and not make up events.
+      Today is ${new Date().toDateString()}.
       Answer the query without hallucinating, and only using the context given.`
     }
   });
